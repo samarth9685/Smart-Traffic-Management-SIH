d743d1ec78adf262e3c3b057062645b490234c2b
samarth9685 <144302485+samarth9685@users.noreply.github.com>
2025-03-02 09:47:35 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+  # image_path = r"C:/Users/samar/OneDrive/Desktop/CODING/Python/Mynewproj/Smart-Traffic-Management/images/right/truck.png"
+        # self.currentImage = pygame.image.load(image_path)
+        # image_path = r"C:/Users/samar/OneDrive/Desktop/CODING/Python/Mynewproj/Smart-Traffic-Management/images/right/car.png"
+        # self.currentImage = pygame.image.load(image_path)
+        # image_path = r"C:/Users/samar/OneDrive/Desktop/CODING/Python/Mynewproj/Smart-Traffic-Management/images/right/rickshaw.png"
+        # self.currentImage = pygame.image.load(image_path)
+        # image_path = r"C:/Users/samar/OneDrive/Desktop/CODING/Python/Mynewproj/Smart-Traffic-Management/images/right/bus.png"
+        # self.currentImage = pygame.image.load(image_path)
+        
